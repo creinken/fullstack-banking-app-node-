29d2819b7acbc7ebdf8dd21570a7d633ed4a87b4
BuildTools <unconfigured@null.spigotmc.org>
2021-05-04 13:17:58 -0500

    created register.js and added registerUser method

---
+++ src/components/Register.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Form, Button } from 'react-bootstrap';
+import { validateFields } from '../utils/common';
+import { Link } from 'react-router-dom';
+
+class Register extends React.Component {
+    state = {
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        cpassword: '',
+        successMsg: '',
+        errorMsg: '',
+        isSubmitted: false
+    };
+
+    registerUser = (event) => {
+        event.preventDefault();
+        const { first_name, last_name, email, password, cpassword } = this.state;
+
+        const fieldsToValidate = [
+            { first_name },
+            { last_name },
+            { email },
+            { password },
+            { cpassword }
+        ];
+    }
+}
