e87b3c40573863fdcd2d50ca8e105d58ddbed6f0
BuildTools <unconfigured@null.spigotmc.org>
2021-05-05 12:11:02 -0500

    completed form and validation on registration page

--- src/components/Register.js
+++ src/components/Register.js
@@ -27,5 +27,108 @@ class Register extends React.Component {
             { password },
             { cpassword }
         ];
+    
+        const allFieldsEntered = validateFields(fieldsToValidate);
+        if (!allFieldsEntered) {
+            this.setState({
+                errorMsg: {
+                    signup_error: 'Please enter all the fields.'
+                }
+            });
+        } else {
+            if (password !== cpassword) {
+                this.setState({
+                    errorMsg: {
+                        signup_error: 'Password and confirm password does not match.'
+                    }
+                });
+            } else {
+                this.setState({ isSubmitted: true });
+            }
+        }
+    }
+
+    handleInputChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    render() {
+        const { errorMsg, successMsg, isSubmitted } = this.state;
+        return (
+            <div className="login-page">
+                <h2>Register User</h2>
+                <div className="login-form">
+                    <Form onSubmit={this.registerUser}>
+                        {errorMsg && errorMsg.signup_error ? (
+                            <p className="errorMsg centered-message">
+                                {errorMsg.signup_error}
+                            </p>
+                        ) : (
+                            isSubmitted && (
+                                <p className="successMsg centered-message">{successMsg}</p>
+                            )
+                        )}
+                        <Form.Group controlId="first_name">
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="first_name"
+                                placeholder="Enter first name"
+                                onChange={this.handleInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="last_name">
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="last_name"
+                                placeholder="Enter last name"
+                                onChange={this.handleInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="email">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="email"
+                                placeholder="Enter email"
+                                onChange={this.handleInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                name="password"
+                                placeholder="Enter password"
+                                onChange={this.handleInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="cpassword">
+                            <Form.Label>Confirm password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                name="cpassword"
+                                placeholder="Enter confirm password"
+                                onChange={this.handleInputChange}
+                            />
+                        </Form.Group>
+                        <div className="action-items">
+                            <Button variant="primary" type="submit">
+                                Register
+                            </Button>
+                            <Link to="/" classname="btn btn-secondary">
+                                Login
+                            </Link>
+                        </div>
+                    </Form>
+                </div>
+            </div>
+        );
     }
 }
+
+export default connect()(Register);
