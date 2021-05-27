35034bb765b36300cbba3753b4f9c7fca6c4cf96
BuildTools <unconfigured@null.spigotmc.org>
2021-05-27 02:04:33 -0500

    added state handler for errors to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import { Form, Button } from 'react-bootstrap';
 import { initiateLogin } from '../actions/auth';
 import { validateFields } from '../utils/common';
 import { Link } from 'react-router-dom';
+import { resetErrors } from '../actions/errors';
 
 class Login extends React.Component {
     state = {
@@ -11,6 +13,16 @@ class Login extends React.Component {
         password: '',
         errorMsg: ''
     };
+
+    componentDidUpdate(prevProps) {
+        if (!_.isEqual(prevProps.errors, this.props.errors)) {
+            this.setState({ errorMsg: this.props.errors });
+        }
+    }
+
+    componentWillUnmount() {
+        this.props.dispatch(resetErrors());
+    }
 
     handleLogin = (event) => {
         event.preventDefault();
@@ -88,4 +100,8 @@ class Login extends React.Component {
     }
 }
 
-export default connect()(Login);
+const mapStateToProps = (state) => ({
+    errors: state.errors
+});
+
+export default connect(mapStateToProps)(Login);
