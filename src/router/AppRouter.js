39b53bfe18b655e6e5c122ca4b21987709ffa281
BuildTools <unconfigured@null.spigotmc.org>
2021-05-28 01:20:13 -0500

    added header if logged in

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -5,18 +5,23 @@ import { Route, Switch, Router } from 'react-router-dom';
 import Login from '../components/Login';
 import Register from '../components/Register';
 import Profile from '../components/Profile';
+import Header from '../components/Header';
+import _ from 'lodash';
 
 export const history = createBrowserHistory();
 
-const AppRouter = () => {
+const AppRouter = ({ auth }) => {
     return (
         <Router history={history}>
-            <div className="container">
-                <Switch>
-                    <Route path="/" component={Login} exact={true} />
-                    <Route path="/register" component={Register} />
-                    <Route path="/profile" component={Profile} />
-                </Switch>
+            <div>
+                {!_.isEmpty(auth.token) && <Header />}
+                <div className="container">
+                    <Switch>
+                        <Route path="/" component={Login} exact={true} />
+                        <Route path="/register" component={Register} />
+                        <Route path="/profile" component={Profile} />
+                    </Switch>
+                </div>
             </div>
         </Router>
     );
