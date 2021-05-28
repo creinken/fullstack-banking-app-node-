cc44b06f1b3840a43d98a1cc0919129164d9b2d5
BuildTools <unconfigured@null.spigotmc.org>
2021-05-28 00:39:54 -0500

    created profile reducer

---
+++ src/reducers/profile.js
@@ -0,0 +1,10 @@
+import { UPDATE_PROFILE } from '../utils/constants';
+
+const profileReducer = (state = {}, action) => {
+    switch (action.type) {
+        case UPDATE_PROFILE:
+            return { ...action.profile };
+        default:
+            return state;
+    }
+};
